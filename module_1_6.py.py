b74eab21e5cb5d5c8ee09d2e18b22bc4485812c6
sarkazi2009 <sarkazi2009@gmail.com>
2024-08-15 05:05:56 +0300

    Add files via upload

---
+++ module_1_6.py.py
@@ -0,0 +1,10 @@
+my_dict = {'Nikolay': 1986, 'Milana': 2021, 'Dasha': 1987}
+print(my_dict)
+print(my_dict['Milana'])
+print(my_dict.get('Dima', 'такого ключа нет'))
+a=my_dict.pop('Dasha')
+print(a)
+my_set = {1, 2, 3, 'Fresh', 3, 3, 21, 'Apple'}
+print(my_set)
+my_set.update(('Pu'), (25,100))
+print(my_set)
